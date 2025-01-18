d3238cab4b99f75014b51c6545fc1e9601df129c
degujoe <129337742+degujoe@users.noreply.github.com>
2025-01-18 23:35:00 +0000

    Update SelectDropDown.tsx already broke

--- client/src/components/ui/SelectDropDown.tsx
+++ client/src/components/ui/SelectDropDown.tsx
@@ -173,7 +173,6 @@ function SelectDropDown({
                     <polyline points="6 9 12 15 18 9"></polyline>
                   </svg>
                 </span>
-              </ListboxButton>
               <Transition
                 show={open}
                 as={React.Fragment}

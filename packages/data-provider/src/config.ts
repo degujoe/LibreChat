85bf236400d2ad993272ccb4a3fed21ebaaccba5
degujoe <129337742+degujoe@users.noreply.github.com>
2025-01-21 22:00:20 +0000

    Update config.ts

--- packages/data-provider/src/config.ts
+++ packages/data-provider/src/config.ts
@@ -12,24 +12,6 @@ export const defaultSocialLogins = ['google', 'facebook', 'openid', 'github', 'd
 
 export const defaultRetrievalModels = [
   'gpt-4o',
-  'o1-preview-2024-09-12',
-  'o1-preview',
-  'o1-mini-2024-09-12',
-  'o1-mini',
-  'chatgpt-4o-latest',
-  'gpt-4o-2024-05-13',
-  'gpt-4o-2024-08-06',
-  'gpt-4o-mini',
-  'gpt-4o-mini-2024-07-18',
-  'gpt-4-turbo-preview',
-  'gpt-3.5-turbo-0125',
-  'gpt-4-0125-preview',
-  'gpt-4-1106-preview',
-  'gpt-3.5-turbo-1106',
-  'gpt-3.5-turbo-0125',
-  'gpt-4-turbo',
-  'gpt-4-0125',
-  'gpt-4-1106',
 ];
 
 export enum SettingsViews {
@@ -623,23 +605,7 @@ export const alternateName = {
 };
 
 const sharedOpenAIModels = [
-  'gpt-4o-mini',
   'gpt-4o',
-  'gpt-3.5-turbo',
-  'gpt-3.5-turbo-0125',
-  'gpt-4-turbo',
-  'gpt-4-turbo-2024-04-09',
-  'gpt-4-0125-preview',
-  'gpt-4-turbo-preview',
-  'gpt-4-1106-preview',
-  'gpt-3.5-turbo-1106',
-  'gpt-3.5-turbo-16k-0613',
-  'gpt-3.5-turbo-16k',
-  'gpt-4',
-  'gpt-4-0314',
-  'gpt-4-32k-0314',
-  'gpt-4-0613',
-  'gpt-3.5-turbo-0613',
 ];
 
 const sharedAnthropicModels = [
